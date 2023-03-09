edbbe0231c98fc100c07b3ee5b5f9607db90fbb0
Yassmine2020 <eddybyassmine@gmail.com>
2023-03-09 20:41:44 +0100

    svm done

--- mnist/part1/svm.py
+++ mnist/part1/svm.py
@@ -5,6 +5,10 @@ from sklearn.svm import LinearSVC
 ### Functions for you to fill in ###
 
 def one_vs_rest_svm(train_x, train_y, test_x):
+    clf = LinearSVC(random_state = 0, C=0.1)
+    clf.fit(train_x, train_y)
+    pred_test_y = clf.predict(test_x)
+    return pred_test_y
     """
     Trains a linear SVM for binary classifciation
 
@@ -19,16 +23,10 @@ def one_vs_rest_svm(train_x, train_y, test_x):
 
 
 def multi_class_svm(train_x, train_y, test_x):
-    """
-    Trains a linear SVM for multiclass classifciation using a one-vs-rest strategy
-
-    Args:
-        train_x - (n, d) NumPy array (n datapoints each with d features)
-        train_y - (n, ) NumPy array containing the labels (int) for each training data point
-        test_x - (m, d) NumPy array (m datapoints each with d features)
-    Returns:
-        pred_test_y - (m,) NumPy array containing the labels (int) for each test data point
-    """
+    clf = LinearSVC(random_state = 0, C=0.1)
+    clf.fit(train_x, train_y)
+    pred_test_y = clf.predict(test_x)
+    return pred_test_y
     raise NotImplementedError
 
 
